fd06db39db92b3262a2b21ab9c33a75d207de2cc
Alexinder Virdee <alex.virdee08@gmail.com>
2019-05-09 23:09:18 -0400

    remove unnecessary js for visbility toggle

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -9,13 +9,9 @@ let clicked = 1;
 
 function toggle_visibility(id, isOdd) {
    let e = document.getElementById(id);
-   document.getElementById("about").style.display = "none";
-   document.getElementById("skills").style.display = "none";
-   document.getElementById("projects").style.display = "none";
+   e.style.display = 'none';
    if (isOdd) {
       e.style.display = "block";
-   } else {
-      e.style.display = 'none';
-   } 
+   }
 }
 
