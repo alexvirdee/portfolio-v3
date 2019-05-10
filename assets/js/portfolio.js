6403d0b647a892e781ef1831ad7cb46556ecaae8
Alexinder Virdee <alex.virdee08@gmail.com>
2019-05-09 21:16:10 -0400

    modulo function button click change div display

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -4,12 +4,18 @@
    document.getElementById('currentYear').innerHTML = year;
 })();
 
+let clicked = 1;
 
-function toggle_visibility(id) {
+
+function toggle_visibility(id, isOdd) {
    let e = document.getElementById(id);
    document.getElementById("about").style.display = "none";
    document.getElementById("skills").style.display = "none";
    document.getElementById("projects").style.display = "none";
-   e.style.display = "block";
+   if (isOdd) {
+      e.style.display = "block";
+   } else {
+      e.style.display = 'none';
+   }
+   
 }
-
