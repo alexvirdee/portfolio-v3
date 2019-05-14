f410815e1e852d040f379670e2895133784fe187
Alexinder Virdee <alex.virdee08@gmail.com>
2019-05-13 20:27:50 -0400

    seperate sections & improved js by removing counter and grabbing div onclick & displaying specific section

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -4,14 +4,33 @@
    document.getElementById('currentYear').innerHTML = year;
 })();
 
-let clicked = 1;
+var sections = ['about', 'skills', 'projects'];
+var sectionDivId = null;
+let currentPos = document.getElementById('current-job-title').innerHTML = 'Web Application Developer';
 
 
-function toggle_visibility(id, isOdd) {
-   let e = document.getElementById(id);
-   e.style.display = 'none';
-   if (isOdd) {
-      e.style.display = "block";
+function sectionVisibility(sectionId) {
+   let init = document.getElementById('init');
+   if (sectionDivId === sectionId) {
+      sectionDivId = null;
+      init.style.display='block';
+   } else {
+      sectionDivId = sectionId;
+      init.style.display='none';
    }
+   hideNonVisibleSections();
 }
 
+
+function hideNonVisibleSections() {
+   var i, sectionId, div;
+   for (i = 0; i < sections.length; i++) {
+      sectionId = sections[i];
+      div = document.getElementById(sectionId);
+      if (sectionDivId === sectionId) {
+         div.style.display = "block";
+      } else {
+         div.style.display = "none";
+      }
+   }
+}
