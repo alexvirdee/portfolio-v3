de380e7fc6b5013f7252d4d81072a3a41ad22b99
Alexinder Virdee <alex.virdee08@gmail.com>
2020-07-13 00:40:23 -0400

    Babel configuration & set webpack to minimize bundle

--- webpack.config.js
+++ webpack.config.js
@@ -12,8 +12,13 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"]
+                test: /\.scss$/,
+                use: ["style-loader", "css-loader", "sass-loader"]
+            },
+            {
+                test: /\.js$/,
+                exclude: '/node_modules/',
+                use: ["babel-loader"]
             }
         ]
     },
